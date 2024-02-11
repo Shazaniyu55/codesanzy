3c7c7a67470d3099281e6dc5b58102048eada3bb
Shazaniyu55 <shazaniyu@gmail.com>
2024-02-11 04:48:02 +0100

    john

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ window.addEventListener('click', function(event) {
         document.querySelector('.navbar').classList.remove('active');
         document.getElementById('menu-btn').classList.remove('active');
     }
-});
+})
+
+window.addEventListener('scroll', function() {
+    var backToTopButton = document.getElementById('back-to-top-btn');
+  
+    if (window.pageYOffset > 300) { // Adjust this value based on your needs
+      backToTopButton.style.display = 'block';
+    } else {
+      backToTopButton.style.display = 'none';
+    }
+  });
+  
+  document.getElementById('back-to-top-btn').addEventListener('click', function() {
+    window.scrollTo(0, 0);
+  });
+  
